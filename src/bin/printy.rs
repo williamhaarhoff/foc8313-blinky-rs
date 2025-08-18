3adf288d57a6bdaf91b7bfca362398a9470f66d3
Will <williamhaarhoff@gmail.com>
2025-08-19 07:03:11 +1200

    Simplified blinky, clock configuration for printy

--- src/bin/printy.rs
+++ src/bin/printy.rs
@@ -7,6 +7,8 @@ use defmt_rtt as _; // <- RTT logging
                     // use defmt_panic as _;
 use embassy_executor::Spawner;
 use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::usart::Uart;
 use embassy_time::{Duration, Timer};
 use panic_probe as _;
 
@@ -18,7 +20,26 @@ fn panic() -> ! {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("🔌 Hello from Embassy STM32!");
-    let p = embassy_stm32::init(Default::default());
+    // configure clocks for 72MHz, with external oscillator
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz::hz(16_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll = Some(Pll {
+            src: PllSource::HSE,
+            prediv: PllPreDiv::DIV2,
+            mul: PllMul::MUL9,
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+        config.rcc.adc_pre = ADCPrescaler::DIV6;
+    }
+    let p = embassy_stm32::init(config);
     let mut led = Output::new(p.PC14, Level::Low, Speed::Low);
 
     loop {
