d38d3ded6903406b0fc9be3b02384653dc7b1169
Will Haarhoff <williamhaarhoff@gmail.com>
2025-10-13 05:31:39 +1300

    Basing Isense on f1 ADC implementation
    Signed-off-by: Will Haarhoff <williamhaarhoff@gmail.com>

--- drivers/src/isense.rs
+++ drivers/src/isense.rs
@@ -1,8 +1,21 @@
+use core::future::poll_fn;
 use core::marker::PhantomData;
+use core::task::Poll;
+use embassy_stm32::adc::AdcChannel;
+use embassy_stm32::gpio::{AfType, Flex, OutputType, Speed};
 use embassy_stm32::interrupt;
+use embassy_stm32::interrupt::typelevel::Interrupt;
 use embassy_stm32::peripherals::ADC1;
-use embassy_stm32::{adc::SampleTime, Peri};
+use embassy_stm32::time::Hertz;
+use embassy_stm32::{adc::SampleTime, rcc, Peri};
 use embassy_sync::waitqueue::AtomicWaker;
+
+#[allow(unused)]
+pub(crate) fn blocking_delay_us(us: u32) {
+    {
+        embassy_time::block_for(embassy_time::Duration::from_micros(us as u64));
+    }
+}
 
 pub struct State {
     pub waker: AtomicWaker,
@@ -39,6 +52,140 @@ pub struct InterruptHandler<T: Instance> {
 impl<T: Instance> interrupt::typelevel::Handler<T::Interrupt> for InterruptHandler<T> {
     unsafe fn on_interrupt() {
         T::state().waker.wake();
+    }
+}
+
+impl<'d, T: Instance> Isense<'d, T> {
+    pub fn new(adc: Peri<'d, T>) -> Self {
+        rcc::enable_and_reset::<T>();
+        T::regs().cr2().modify(|reg| reg.set_adon(true));
+
+        // 11.4: Before starting a calibration, the ADC must have been in power-on state (ADON bit = ‘1’)
+        // for at least two ADC clock cycles.
+        blocking_delay_us((1_000_000 * 2) / Self::freq().0 + 1);
+
+        // Reset calibration
+        T::regs().cr2().modify(|reg| reg.set_rstcal(true));
+        while T::regs().cr2().read().rstcal() {
+            // spin
+        }
+
+        // Calibrate
+        T::regs().cr2().modify(|reg| reg.set_cal(true));
+        while T::regs().cr2().read().cal() {
+            // spin
+        }
+
+        // One cycle after calibration
+        blocking_delay_us((1_000_000 * 1) / Self::freq().0 + 1);
+
+        T::Interrupt::unpend();
+        unsafe { T::Interrupt::enable() };
+
+        Self {
+            adc,
+            sample_time: SampleTime::from_bits(0),
+        }
+    }
+
+    fn freq() -> Hertz {
+        rcc::frequency::<T>()
+    }
+
+    pub fn sample_time_for_us(&self, us: u32) -> SampleTime {
+        match us * Self::freq().0 / 1_000_000 {
+            0..=1 => SampleTime::CYCLES1_5,
+            2..=7 => SampleTime::CYCLES7_5,
+            8..=13 => SampleTime::CYCLES13_5,
+            14..=28 => SampleTime::CYCLES28_5,
+            29..=41 => SampleTime::CYCLES41_5,
+            42..=55 => SampleTime::CYCLES55_5,
+            56..=71 => SampleTime::CYCLES71_5,
+            _ => SampleTime::CYCLES239_5,
+        }
+    }
+
+    //pub fn enable_vref(&self) -> Vref {
+    //    T::regs().cr2().modify(|reg| {
+    //        reg.set_tsvrefe(true);
+    //    });
+    //    Vref {}
+    //}
+
+    //pub fn enable_temperature(&self) -> Temperature {
+    //    T::regs().cr2().modify(|reg| {
+    //        reg.set_tsvrefe(true);
+    //    });
+    //    Temperature {}
+    //}
+
+    pub fn set_sample_time(&mut self, sample_time: SampleTime) {
+        self.sample_time = sample_time;
+    }
+
+    /// Perform a single conversion.
+    async fn convert(&mut self) -> u16 {
+        T::regs().cr2().modify(|reg| {
+            reg.set_adon(true);
+            reg.set_swstart(true);
+        });
+        T::regs().cr1().modify(|w| w.set_eocie(true));
+
+        poll_fn(|cx| {
+            T::state().waker.register(cx.waker());
+
+            if !T::regs().cr2().read().swstart() && T::regs().sr().read().eoc() {
+                Poll::Ready(())
+            } else {
+                Poll::Pending
+            }
+        })
+        .await;
+
+        T::regs().dr().read().0 as u16
+    }
+
+    // pub async fn read(&mut self, channel: &mut impl AdcChannel<T>) -> u16 {
+    //     Self::set_channel_sample_time(channel.channel(), self.sample_time);
+    //     T::regs().cr1().modify(|reg| {
+    //         reg.set_scan(false);
+    //         reg.set_discen(false);
+    //     });
+    //     T::regs().sqr1().modify(|reg| reg.set_l(0));
+
+    //     T::regs().cr2().modify(|reg| {
+    //         reg.set_cont(false);
+    //         reg.set_exttrig(true);
+    //         reg.set_swstart(false);
+    //         reg.set_extsel(7); // SWSTART
+    //     });
+
+    //     // Configure the channel to sample
+    //     T::regs()
+    //         .sqr3()
+    //         .write(|reg| reg.set_sq(0, channel.channel()));
+    //     self.convert().await
+    // }
+
+    fn set_channel_sample_time(ch: u8, sample_time: SampleTime) {
+        let sample_time = sample_time.into();
+        if ch <= 9 {
+            T::regs()
+                .smpr2()
+                .modify(|reg| reg.set_smp(ch as _, sample_time));
+        } else {
+            T::regs()
+                .smpr1()
+                .modify(|reg| reg.set_smp((ch - 10) as _, sample_time));
+        }
+    }
+}
+
+impl<'d, T: Instance> Drop for Isense<'d, T> {
+    fn drop(&mut self) {
+        T::regs().cr2().modify(|reg| reg.set_adon(false));
+
+        rcc::disable::<T>();
     }
 }
 
---
+++ foc8313-blinky/src/bin/isense_driver.rs
@@ -0,0 +1,77 @@
+#![no_std]
+#![no_main]
+use defmt::*;
+use drivers::isense::Isense;
+use drivers::pwm::{CompareOC4, Phase, Pwm3};
+use embassy_executor::Spawner;
+use embassy_stm32::time::{hz, khz, Hertz};
+use embassy_stm32::{
+    gpio::{Level, Output, Speed},
+    interrupt,
+};
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+
+#[defmt::panic_handler]
+fn panic() -> ! {
+    cortex_m::asm::udf()
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("🔌 Hello from Embassy STM32!");
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        config.rcc.hse = Some(Hse {
+            freq: Hertz::hz(16_000_000),
+            mode: HseMode::Oscillator,
+        });
+        config.rcc.pll = Some(Pll {
+            src: PllSource::HSE,
+            prediv: PllPreDiv::DIV2,
+            mul: PllMul::MUL9,
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+        config.rcc.adc_pre = ADCPrescaler::DIV6;
+    }
+    let p = embassy_stm32::init(config);
+
+    let mut enable_pin = Output::new(p.PB1, Level::Low, Speed::Low);
+    //let mut led = Output::new(p.PC14, Level::Low, Speed::Low);
+    enable_pin.set_high();
+
+    let mut isense_driver = Isense::new(p.ADC1);
+
+    let mut pwm_driver = Pwm3::new(p.TIM3, p.PA6, p.PA7, p.PB0, CompareOC4, khz(16));
+    pwm_driver.enable(Phase::A);
+    pwm_driver.enable(Phase::B);
+    pwm_driver.enable(Phase::C);
+    let duty = pwm_driver.get_max_duty() / 16;
+    let off: u16 = 0;
+
+    unsafe {
+        cortex_m::peripheral::NVIC::unmask(interrupt::TIM3);
+    }
+
+    let sequence = [
+        (duty, off, off),
+        (duty, duty, off),
+        (off, duty, off),
+        (off, duty, duty),
+        (off, off, duty),
+        (duty, off, duty),
+    ];
+
+    loop {
+        for (a, b, c) in sequence.iter() {
+            pwm_driver.set_duty(Phase::A, *a);
+            pwm_driver.set_duty(Phase::B, *b);
+            pwm_driver.set_duty(Phase::C, *c);
+            Timer::after_millis(50).await;
+        }
+    }
+}
